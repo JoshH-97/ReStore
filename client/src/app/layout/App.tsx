b41aedc9258d90d911cfb0985e629b29277f189d
Josh.Harris <josh.harris@evolve-consultants.co.uk>
2024-03-28 00:16:33 +0000

    End of section 6

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,11 +1,30 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Header from "./Header";
 import { Container, CssBaseline, ThemeProvider, createTheme } from "@mui/material";
 import { Outlet } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import 'react-toastify/ReactToastify.css'
+import { useStoreContext } from "../context/StoreContext";
+import { getCookie } from "../util/util";
+import agent from "../api/agent";
+import LoadingComponent from "./LoadingComponent";
 
 function App() {
+  const {setBasket} = useStoreContext();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(()=> {
+    const buyerId = getCookie('buyerId');
+    if (buyerId){
+      agent.Basket.get()
+      .then(basket => setBasket(basket))
+      .catch(error => console.log(error))
+      .finally(() => setLoading(false))
+    } else {
+      setLoading(false);
+    }
+  }, [setBasket])
+
   const [darkmode, setDarkMode] = useState(false);
   const paletteType = darkmode ? 'dark' : 'light'
   const theme = createTheme({
@@ -21,6 +40,8 @@ function App() {
     setDarkMode(!darkmode)
   }
 
+  if (loading) return <LoadingComponent message="Initialising App"/>
+
   return (
     <ThemeProvider theme={theme}>
       <ToastContainer position="bottom-right" hideProgressBar theme="colored" />
@@ -29,7 +50,7 @@ function App() {
       <Container>
         <Outlet />
       </Container>
-    </ThemeProvider>
+  </ThemeProvider>
   );
 }
 
